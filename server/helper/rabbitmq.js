85562eb91873f7ff5f10763215efad8201f394f9
Oguazu Stanley <deareststanley@gmail.com>
2023-04-01 21:35:32 +0100

    minor refactor

--- server/helper/rabbitmq.js
+++ server/helper/rabbitmq.js
@@ -1,12 +1,22 @@
-import * as amqp from 'amqplib/callback_api';
+const amqp = require('amqplib');
 
 const CONN_URL = process.env.RABBITMQ_SERVICE;
 let ch = null;
-amqp.connect(CONN_URL, function (err, conn) {
-  conn.createChannel(function (err, channel) {
-    ch = channel;
-  });
+// amqp.connect(CONN_URL, function (err, conn) {
+//   conn.createChannel(function (err, channel) {
+//     ch = channel;
+//   });
+// });
+async function producer() {
+  const connection = await amqp.connect(CONN_URL);
+  let channel = await connection.createChannel();
+  return channel;
+}
+
+producer().then((channel) => {
+  ch = channel;
 });
+
 export const publishToQueue = async (queueName, data) => {
   await ch.assertQueue(queueName, { durable: false });
   await ch.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
